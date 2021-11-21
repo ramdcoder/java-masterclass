46027fe6d9ac06dd84844d8faf582c76aae287bf
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-21 13:41:14 -0600

    started java masterclass

---
+++ src/HelloWorld/Hello.java
@@ -0,0 +1,15 @@
+package HelloWorld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int firstNumber = 5;
+        System.out.println("firstNumber = " + firstNumber);
+        int secondNumber;
+
+        secondNumber = firstNumber - 2;
+
+        System.out.println(secondNumber);
+    }
+}
