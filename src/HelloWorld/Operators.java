f6dcb1ee4a55f012bd2c40c81fd2d8d7122c2535
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-28 01:53:17 -0600

    working with ternary operators in java

--- src/HelloWorld/Operators.java
+++ src/HelloWorld/Operators.java
@@ -2,7 +2,31 @@ package HelloWorld;
 
 public class Operators {
     public static void main(String[] args) {
-        int result = 1 + 2;
+        // practice with ternary
+//        boolean isSingle = false;
+//        isSingle = true;
+//
+//        boolean wasSingle = isSingle ? true : false;
+//
+//        if (wasSingle) {
+//            System.out.println("Was single is true");
+//        }
+//
+//        boolean isEmployed = false;
+//        boolean hasJob = (isEmployed == true) ? true : false;
+        double num1 = 20.00;
+        double num2 = 80.00;
+        double sumAndMultiply = (num1 + num2) * 100;
+        double remainder = sumAndMultiply % 40;
+//        System.out.println("remainder = " + remainder);
 
+//        boolean remainderIsZero = (remainder == 0) ? true : false;
+        boolean remainderIsZero = remainder == 0;
+
+        if (remainderIsZero) {
+            System.out.println("remainderIsZero = " + remainderIsZero);
+        } else {
+            System.out.println("Some remainder");
+        }
     }
 }
