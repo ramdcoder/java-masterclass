a5dba618cf17dfdddaad4faeaa9fd44224d8b151
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-28 12:47:13 -0600

    adjusted conversion variable

--- src/CodingExercises/SpeedConverter.java
+++ src/CodingExercises/SpeedConverter.java
@@ -3,7 +3,7 @@ package CodingExercises;
 public class SpeedConverter {
 
     public static long toMilesPerHour (double kilometersPerHour) {
-        double oneKmhInMph = 1.609344;
+        double oneKmhInMph = 1.609;
         if (kilometersPerHour < 0) {
             return -1;
         }
@@ -14,7 +14,7 @@ public class SpeedConverter {
         if (kilometersPerHour < 0) {
             System.out.printf("Invalid Value%n");
         } else {
-            System.out.printf("%s kmh = %s mph%n", kilometersPerHour, toMilesPerHour(kilometersPerHour));
+            System.out.printf("%s km/h = %s mi/h%n", kilometersPerHour, toMilesPerHour(kilometersPerHour));
         }
     }
 
